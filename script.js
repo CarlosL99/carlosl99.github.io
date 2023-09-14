54fafd16df0d15c20a5379db2a6a02283a7724fd
Ezequiel Lazo <ezze_car@hotmail.com>
2023-09-14 10:12:31 -0300

    Cambios

--- script.js
+++ script.js
@@ -15,20 +15,3 @@ function seleccionar() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-
-const DescargarCurriculum = document.querySelector("#Btn_Descargar");
-
-DescargarCurriculum.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    // URL del archivo que deseas descargar
-    const urlArchivo = '/Mi Curriculum Develop 2.0.pdf';
-
-    const enlace = document.createElement('a');
-    enlace.href = urlArchivo;
-    enlace.target = '_blank';
-    enlace.download = 'Curriculum_Lassorela.pdf';
-
-    // Simula un clic en el enlace para iniciar la descarga
-    enlace.click();
-});
